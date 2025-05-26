c49b2c7f7d6b5022d79ac13e5d3d86f61a2b7d61
Desar <desar@sariyanta.com>
2025-05-26 09:06:50 +0200

    test this

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!' + process.env['MY_SECRET'];
+    return (
+      'Hello World!' +
+      ' ' +
+      process.env['MY_SECRET'] +
+      ' ' +
+      process.env['NODE_ENV']
+    );
   }
 }
