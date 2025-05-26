ffcc91b90070813a6ea723facfc4d68adfce8d58
Desar <desar@sariyanta.com>
2025-05-26 22:47:42 +0200

    fix build script

--- apps/client/src/routes/index.tsx
+++ apps/client/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { HeadContent, createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: App,
