439c53225965be531322ef571dc5adaa2d77eb9a
Desar <desar@sariyanta.com>
2025-06-01 16:49:15 +0200

    api: chore correctly setup import plugin

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -3,11 +3,13 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import * as importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
+  importPlugin?.flatConfigs?.recommended,
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
@@ -17,7 +19,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 2020,
       sourceType: 'module',
       parserOptions: {
         projectService: true,
@@ -25,12 +27,28 @@ export default tseslint.config(
       },
     },
   },
-
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      'sort-imports': 'off',
+      'import/no-unresolved': 'off',
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+        },
+      ],
     },
   },
 );
