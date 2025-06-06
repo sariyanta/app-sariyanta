c68759359e724f3e89bb1887344727c90afc7508
Desar <desar@sariyanta.com>
2025-06-06 09:08:44 +0200

    (www) replaces /project with / on navigation

--- apps/www/src/components/global/navigation.tsx
+++ apps/www/src/components/global/navigation.tsx
@@ -11,15 +11,33 @@ import {
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 
+interface NavigationItem {
+  title: string;
+  href: string;
+  children?: NavigationItem[];
+}
+
+const navigations: NavigationItem[] = [
+  {
+    title: 'Home',
+    href: '/',
+  },
+];
+
 export function NavigationMenuDemo() {
   return (
     <NavigationMenu viewport={false}>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/projects">Projects</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+        {navigations.map((item) => (
+          <NavigationMenuItem key={item.href}>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href={item.href}>{item.title}</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
       </NavigationMenuList>
     </NavigationMenu>
   );
